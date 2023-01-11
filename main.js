e3b4f65e160b40c78a35583602326669787aa341
Phương Thảo <93078644+huutri3110@users.noreply.github.com>
2023-01-11 21:53:31 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ const ctx = canvas.getContext('2d');
 
 const ROW = 18;
 const COL = 10;
-const SQ  = 40;
+const SQ = 40;
 const COLOR = "WHITE";
 let score = 0;
 let speed = 1000;
 
-function drawSquare (x, y, color) {
+function drawSquare(x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect (x*SQ, y*SQ, SQ, SQ);
+  ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
 
   ctx.strokeStyle = "#ccc";
-  ctx.strokeRect (x*SQ, y*SQ, SQ, SQ);
+  ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
 }
 
 let board = [];
@@ -24,12 +24,12 @@ for (r = 0; r < ROW; r++) {
   }
 }
 
-console.log (board);
+// console.log (board);
 
-function drawBoard () {
+function drawBoard() {
   for (r = 0; r < ROW; r++) {
     for (c = 0; c < COL; c++) {
-      drawSquare (c, r, board[r][c]);
+      drawSquare(c, r, board[r][c]);
     }
   }
 }
@@ -37,7 +37,7 @@ function drawBoard () {
 drawBoard();
 
 class Piece {
-  constructor (tetromino, color) {
+  constructor(tetromino, color) {
     this.tetromino = tetromino;
     this.color = color;
 
@@ -52,48 +52,48 @@ class Piece {
     for (let r = 0; r < this.activeTetromino.length; r++) {
       for (let c = 0; c < this.activeTetromino.length; c++) {
         if (this.activeTetromino[r][c]) {
-          drawSquare (this.x + c, this.y + r, color);
+          drawSquare(this.x + c, this.y + r, color);
         }
       }
     }
   }
 
-  draw () {
-    this.fill (this.color);
+  draw() {
+    this.fill(this.color);
   }
 
-  unDraw () {
-    this.fill (COLOR);
+  unDraw() {
+    this.fill(COLOR);
   }
 
-  moveDown () {
-    if (!this.collision (0, 1, this.activeTetromino)) {
-      this.unDraw ();
+  moveDown() {
+    if (!this.collision(0, 1, this.activeTetromino)) {
+      this.unDraw();
       this.y++;
-      this.draw ();
+      this.draw();
     } else {
       this.lock();
-      p = randomPiece ();
+      p = randomPiece();
     }
   }
 
-  moveLeft () {
-    if (!this.collision (-1, 0, this.activeTetromino)) {
-      this.unDraw ();
+  moveLeft() {
+    if (!this.collision(-1, 0, this.activeTetromino)) {
+      this.unDraw();
       this.x--;
-      this.draw ();
+      this.draw();
     }
   }
 
-  moveRight () {
-    if (!this.collision (1, 0, this.activeTetromino)) {
-      this.unDraw ();
+  moveRight() {
+    if (!this.collision(1, 0, this.activeTetromino)) {
+      this.unDraw();
       this.x++;
-      this.draw ();
+      this.draw();
     }
   }
 
-  lock () {
+  lock() {
     for (let r = 0; r < this.activeTetromino.length; r++) {
       for (let c = 0; c < this.activeTetromino.length; c++) {
         if (!this.activeTetromino[r][c]) {
@@ -101,7 +101,11 @@ class Piece {
         }
 
         if (this.y + r < 0) {
-          alert ("GAME OVER!");
+          // alert("GAME OVER!");
+          document.querySelector(".menuEnd").style.display = "block";
+          document.addEventListener('keydown', function (e) {
+            e.preventDefault();
+          })
           gameOver = true;
           break;
         }
@@ -120,52 +124,59 @@ class Piece {
 
       if (isFull) {
         for (let y = r; y > 1; y--) {
-          for (let c = 0; c< COL; c++) {
+          for (let c = 0; c < COL; c++) {
             board[y][c] = board[y - 1][c];
           }
         }
 
-        for (let c = 0; c< COL; c++) {
+        for (let c = 0; c < COL; c++) {
           board[0][c] = COLOR;
         }
         score += 10;
         if (score % 40 === 0) {
-          speed -= 200;
-          drop ();
+          speed -= 150;
+          drop();
         }
       }
     }
-    drawBoard ();
+    drawBoard();
     document.querySelector('#score').innerText = score;
   }
 
-  rotate () {
+  rotate() {
     let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
 
+
+
+
     let move = 0;
-    if (this.collision (0, 0, nextPattern)) {
-      if (this.x > COL / 2) {
-        move = -1;
-      } else {
-        move = 1;
-      }
+    if (this.collision(0, 0, nextPattern)) {
+
+      do {
+        this.unDraw();
+        if (this.x > COL / 2) {
+          move = -1;
+        } else {
+          move = 1;
+        }
+
+        this.x += move;
+      } while (this.collision(0, 0, nextPattern));
     }
 
-    if (!this.collision (0, 0, nextPattern)) {
-      this.unDraw ();
-      this.x += move;
-      this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
-      this.activeTetromino = this.tetromino [this.tetrominoN];
-      this.draw ();
-    }
+    this.unDraw();
+    this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length;
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    this.draw();
+
   }
 
-  collision (x, y, piece) {
+  collision(x, y, piece) {
     for (let r = 0; r < piece.length; r++) {
       for (let c = 0; c < piece.length; c++) {
         if (!piece[r][c]) {
           continue;
-        } 
+        }
 
         let newX = this.x + c + x;
         let newY = this.y + r + y;
@@ -199,37 +210,44 @@ const PIECES = [
   [J, "orange"],
 ];
 
-function randomPiece () {
+function randomPiece() {
   let r = Math.floor(Math.random() * PIECES.length);
-  return new Piece (PIECES[r][0], PIECES[r][1]);
+  return new Piece(PIECES[r][0], PIECES[r][1]);
 }
 
 let p = randomPiece();
-console.log (p);
+console.log(p);
 
-document.addEventListener ('keydown', function (e) {
+document.addEventListener('keydown', function (e) {
   if (e.key == 'ArrowLeft') {
-    p.moveLeft ();
+    p.moveLeft();
   } else if (e.key == 'ArrowRight') {
-    p.moveRight ();
+    p.moveRight();
   } else if (e.key == 'ArrowUp') {
-    p.rotate ();
+    p.rotate();
   } else if (e.key == 'ArrowDown') {
-    p.moveDown ();
+    p.moveDown();
   }
 })
 
 let gameOver = false;
 let interval;
+const menuStart = document.querySelector(".menuStart");
 
-function drop () {
-  interval = setInterval (function () {
+function drop() {
+  interval = setInterval(function () {
     if (!gameOver) {
-      p.moveDown ();
+      p.moveDown();
     } else {
-      clearInterval (interval);
+      clearInterval(interval);
     }
   }, speed)
 }
 
-drop ();
+
+menuStart.addEventListener("click", function (e) {
+  document.querySelector (".overLay").style.display = "none";
+  menuStart.style.display = "none";
+  drop();
+});
+
